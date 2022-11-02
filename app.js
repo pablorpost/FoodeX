8c2132c0b4c41d8da55c8607014d4e53a3a41db3
daaaviid-03 <d.pimentel.2021@alumnos.urjc.es>
2022-11-02 17:59:01 +0100

    imagenes en la BD, images -> array
    imagenes en la BD, images -> array

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Lista predefinida de 
 let predefinedRecipes = [
-    ['Arroz con Leche', '', '',
+    ['Arroz con Leche', '', ['Resources/arrozConLeche1.jpg', 'Resources/arrozConLeche2.jpg', 'Resources/arrozConLeche3.jpg'],
         ['1 litro de Leche', '3 cucharadas de arroz', '4 cucharadas de Azúcar', 'Piel de ¼ de limón', 'Canela en rama'],
         'Se hierve todo junto lentamente moviendo continuamente durante 40 min.'],
 
-    ['Bizcocho de Yogurt', '', '',
+    ['Bizcocho de Yogurt', '', ['Resources/bizcochoDeYogurt1.jpg', 'Resources/bizcochoDeYogurt2.jpg', 'Resources/bizcochoDeYogurt3.jpg'],
         ['1 yogurt de limón', '½ medida de aceite', '2 medidas de azúcar', '3 medidas de harina', '4 huevos', '½ sobre de levadura', 'Sal'],
         `Se baten los huevos con sal.
     Se añade el yogurt, el aceite y el azúcar.
@@ -12,7 +12,7 @@ let predefinedRecipes = [
     
     Hornear a 175 ºC durante 20 min.`],
 
-    ['Tortitas', '', '',
+    ['Tortitas', '', ['Resources/tortitas1.jpg', 'Resources/tortitas2.jpg'],
         ['2 huevos', '1 cucharada de azúcar', '1 cucharada de aceite', '1,5 vasos de leche', '200 gr de harina (6 cucharadas grandes)', 'Levadura', 'Sal'],
         `Se baten los huevos con sal.
     Se añade el azúcar, el aceite y la leche y se bate.
@@ -28,14 +28,14 @@ class Recipe {
 
     name = ''
     description = ''
-    image = ''
+    images = new Array()
     ingredients = new Array()
     preparation = ''
 
     constructor(datos) {
         if (datos[0]) { this.name = datos[0] }
         if (datos[1]) { this.description = datos[1] }
-        if (datos[2]) { this.image = datos[2] }
+        if (datos[2]) { this.images = datos[2] }
         if (datos[3]) { this.ingredients = datos[3] }
         if (datos[4]) { this.preparation = datos[4] }
     }
@@ -54,10 +54,10 @@ class Recipe {
         this.description = description
     }
 
-    getImage(){
+    getImages(){
         return this.image
     }
-    setImage(image){
+    setImages(image){
         this.image = image
     }
 
