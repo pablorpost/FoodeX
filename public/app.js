6322705d34d21bb9a51e9ea6b41cfc81cd314096
daaaviid-03 <d.pimentel.2021@alumnos.urjc.es>
2022-12-16 12:53:46 +0100

    cacota

--- public/app.js
+++ public/app.js
@@ -2,10 +2,11 @@ let loadMoreIngredientsRequests = 0;
 
 async function loadMoreIngredients(){
 
+    console.log("ouhdsihiuvhocfdnjdvgfbniedfnwsea");
     const response = await fetch(`/newIngredient`);
 
     const newIngredient = await response.text();
-  
+    console.log(newIngredient);
     const ingredientsUl = document.getElementById("lista_ingredientes");
 
     ingredientsUl.innerHTML += newIngredient;
