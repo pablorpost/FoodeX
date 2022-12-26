020a882e2b62dfecab1125cf199b56f76b96c453
Pablo R. P <pablorepost@gmail.com>
2022-12-26 22:10:49 +0100

    ª

--- public/app.js
+++ public/app.js
@@ -3,16 +3,11 @@ let loadMoreRequests = 0;
 let loadMoreIngredientsRequests = 0;
 
 async function loadMoreIngredients(){
-
-    console.log("ouhdsihiuvhocfdnjdvgfbniedfnwsea");
     const response = await fetch(`/newIngredient`);
-
     const newIngredient = await response.text();
     console.log(newIngredient);
     const ingredientsUl = document.getElementById("lista_ingredientes");
-
     ingredientsUl.innerHTML += newIngredient;
-
     loadMoreIngredientsRequests++;
 }
 
