e2da32a62ea4714d891297242a8a34fafeae8998
Pablo R. P <pablorepost@gmail.com>
2023-01-02 19:28:18 +0100

    functioning

--- src/boardRouter.js
+++ src/boardRouter.js
@@ -53,11 +53,85 @@ router.get('/showMore/:id/trudelete', (req, res) => {
     recipesService.deleteRecipe(req.params.id)
     res.redirect('/')
 });
+/*
+router.get('/showMore/:id/edit', (req, res) => {
+    res.render('add',{ 
+        recipe: recipesService.getRecipe(req.params.id)
+    });
+});
 
-function getRecipeFromForm(reqBody){
-    let name = reqBody.nombre;
-    let description = reqBody.description;
-    let ingred = reqBody.ingred;
+router.post('/showMore/:id/edit/add', (req, res) => {
+    console.log('------------------------------------------------------------------')
+    console.log(req.body);
+    let {recipeName, recipeDescription , recipeSteps , recipeIngredients, recipePhotos} = req.body;
+    recipesService.editRecipe(req.params.id, {recipeName, recipeDescription , recipePhotos, recipeIngredients, recipeSteps });
+    res.redirect('/');
+});
+*/
+router.post('/recipe/new', (req, res) => {
+    
+    let name = req.body.nombre;
+    let description = req.body.description;
+    let ingred = req.body.ingred;
+    let images = req.body.imag;
+    let ingredMap = new Map();
+    let sum = 0;
+    if (ingred) {
+        for(const l of ingred){sum += l.length}
+        if (ingred.length!=sum) {
+            for (let i = 0; i < ingred.length; i++) {
+                ingredMap.set(i, ingred[i]);
+            }
+        } else {
+            ingredMap.set(0, ingred);
+        }
+    }
+
+    let steps = req.body.steps;
+    let stepMap = new Map();
+    sum = 0
+    if (steps) {
+        for(const l of steps){sum += l.length}
+        if (steps.length!=sum) {
+            for (let i = 0; i < steps.length; i++) {
+                stepMap.set(i, steps[i]);
+            }
+        } else {
+            stepMap.set(0, steps);
+        }
+    }
+
+    let imagesMap = new Map();
+    sum = 0;
+    if(images){
+        for(const l of images){sum += l.length}
+        if (images.length!=sum) {
+            for (let i = 0; i < images.length; i++) {
+                imagesMap.set(i, images[i]);
+            }
+        } else {
+            imagesMap.set(0, images);
+        }
+    }
+    else{
+        imagesMap.set(0, '/Resources/fotoPredeterminadaDeReceta.jpg');
+    }
+
+    recipesService.addRecipe([name, description, imagesMap, ingredMap, stepMap]);
+    res.redirect('/');
+});
+
+router.get('/showMore/:id/edit', (req, res) => {
+    // crear la clase con la info
+    let thisRecipe = recipesService.getRecipe(req.params.id);
+    res.render('edit_recipe', thisRecipe);
+});
+
+router.post('/showMore/:id/edit', (req, res) => {
+    
+    let name = req.body.nombre;
+    let description = req.body.description;
+    let ingred = req.body.ingred;
     let ingredMap = new Map();
     let sum = 0;
     if (ingred) {
@@ -88,23 +162,8 @@ function getRecipeFromForm(reqBody){
     
     let images = new Map();
     images.set(0, '/Resources/fotoPredeterminadaDeReceta.jpg');
-    return [name, description, images, ingredMap, stepMap]
-}
-
-router.post('/recipe/new', (req, res) => {
-    recipesService.addRecipe(getRecipeFromForm(req.body));
-    res.redirect('/');
-});
-
-router.get('/showMore/:id/edit', (req, res) => {
-    // crear la clase con la info
-    let thisRecipe = recipesService.getRecipe(req.params.id);
-    res.render('edit_recipe', thisRecipe);
-});
-
-router.post('/showMore/:id/edit', (req, res) => {
-
-    recipesService.editRecipe(req.params.id, getRecipeFromForm(req.body));
+    
+    recipesService.editRecipe(req.params.id, [name, description, images, ingredMap, stepMap]);
     res.redirect('/');
 });
 
